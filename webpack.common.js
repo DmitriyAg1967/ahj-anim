2ac6dedef083e97ed5e806c73fc5a8d342f272f7
Dmitriy <aglerimov05@mail.ru>
2023-01-30 23:32:55 +0300

    Второй коммит

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   output: {
